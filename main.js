b259fe2050f2fb84cdd94da7c66f17498e277c66
Hazrat Ali <iloveyii@yahoo.com>
2019-10-29 23:20:26 +0100

    Added class Blockchain

--- main.js
+++ main.js
@@ -5,12 +5,37 @@ class Block {
         this.index = index;
         this.timestamp = timestamp;
         this.data = data;
-        this.priviousHash = '';
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
     }
 
     calculateHash() {
-        return SHA256(this.index + this.timestamp + JSON.stringify(this.data) + this.priviousHash).toString();
+        return SHA256(this.index + this.timestamp + JSON.stringify(this.data) + this.previousHash).toString();
     }
 }
 
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
 
+    createGenesisBlock() {
+        return new Block(0, '2019/10/28', 'Genesis Block', "00")
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+}
+
+let alSaeedCoin = new Blockchain();
+alSaeedCoin.addBlock(new Block(1, '2019/10/29', {amount: 5}));
+alSaeedCoin.addBlock(new Block(2, '2019/10/30', {amount: 15}));
+
+console.log(JSON.stringify(alSaeedCoin, null, 4));
